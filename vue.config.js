601bd4a6e583b123b6ce78edee4811294bdfaa20
lanlantu <zhangxi_830@163.com>
2022-04-25 10:41:16 +0800

    proxy

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://cloud-music-api-red-rho.vercel.app',
         // target: 'https://cloud-music-api-red-rho.vercel.app',
         changeOrigin: true,
         pathRewrite: {
